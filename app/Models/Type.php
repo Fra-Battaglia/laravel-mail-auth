5cc5a4a45fc0b834c7f3c8a8f96b96bfaa529905
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-03-08 18:37:54 +0100

    type slug added

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use illuminate\Support\Str;
 
 class Type extends Model
 {
     protected $guarded = [];
     use HasFactory;
+
+    public static function generateSlug($val)
+    {
+        return Str::slug($val, '-');
+    }
 
     public function project() {
         return $this->hasMany(Project::class);
