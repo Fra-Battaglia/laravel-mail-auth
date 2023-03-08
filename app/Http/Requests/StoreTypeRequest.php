3804b5422118bafc74ba0284e43204297c7e7e64
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-03-08 18:40:22 +0100

    store type fixed

--- app/Http/Requests/StoreTypeRequest.php
+++ app/Http/Requests/StoreTypeRequest.php
@@ -24,7 +24,7 @@ class StoreTypeRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|unique:projects|max:100',
+            'name' => 'required|unique:types|max:100',
         ];
     }
 
